ea1c8a3428997223b898079a191b00a47ca690f0
Joey <joey831201@gmail.com>
2020-02-22 20:52:26 +0800

    4rd commit

--- vue.confing.js
+++ vue.confing.js
@@ -1,10 +1,10 @@
 // vue.confing.js
-// module.exports = {
-//   publicPath: process.env.NODE_ENV === 'maskmapvue'
-//     ? '/maskmapvue/'
-//     : '/',
-// };
-
 module.exports = {
-  publicPath: '',
+  publicPath: process.env.NODE_ENV === 'maskmapvue'
+    ? '/maskmapvue/'
+    : '/',
 };
+
+// module.exports = {
+//   publicPath: '',
+// };
